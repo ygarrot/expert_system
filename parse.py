7356866a85cb2736809f6104caf32be652e6197a
gbrochar <gbrochar@student.42.fr>
2019-02-15 20:21:51 +0100

    Parser upgrade

--- parse.py
+++ parse.py
@@ -1,33 +1,35 @@
-fact_dict = {}
-
 from lark import Lark, Transformer, v_args
-
 
 try:
     input = raw_input   # For Python2 compatibility
 except NameError:
     pass
 
-calc_grammar = """
-    ?start: assign
-    ?assign: xor
-        | xor "=>" LETTER -> assign_var
-    ?xor: or
-        | and "^" atom  -> xor
-    ?or: and
-        | and "|" atom   -> or
-    ?and: atom
-        | and "+" atom   -> and
-    ?atom: LETTER        -> var
-         | "!" atom      -> not
-         | "(" assign ")" 
-    ?query: "?" LETTER+
-    ?initial_fact: "=" LETTER+
+fact_dict = {}
 
-    %import common.LETTER
+calc_grammar = """
+    ?start: imply | initial_fact | query
+
+    ?imply: xor "=>" xor -> imply
+        | xor "<=>" xor -> iff
+    ?xor: or
+        | xor "^" xor  -> xor
+    ?or: and
+        | xor "|" xor   -> or
+    ?and: atom
+        | xor "+" xor   -> and
+    ?atom: UCASE_LETTER        -> var
+         | "!" atom      -> not
+         | "(" xor ")"
+
+    ?initial_fact: "=" UCASE_LETTER+
+    ?query: "?" UCASE_LETTER+
+
+    %import common.UCASE_LETTER
     %import common.NUMBER
     %import common.WS_INLINE
     %ignore WS_INLINE
+    %ignore /#.*/
 """
 
 @v_args(inline=True)    # Affects the signatures of the methods
@@ -37,7 +39,7 @@ class CalculateTree(Transformer):
 
     def set_fact(self, string):
         for letters in string:
-          fact_dict[letters] = True  
+          fact_dict[letters] = True
 
     def get_fact_state(self, string):
         for letters in string:
@@ -52,6 +54,10 @@ class CalculateTree(Transformer):
            fact_dict[name] = 0
         return fact_dict[name]
 
+calc_parser = Lark(calc_grammar, parser='lalr')
+#, transformer=CalculateTree()) # Cheat ?
+calc = calc_parser.parse
+
 def run_instruction(t):
     try:
         print(t.data)
@@ -59,10 +65,6 @@ def run_instruction(t):
             run_instruction(instr)
     except:
          return
-
-calc_parser = Lark(calc_grammar, parser='lalr')
-#, transformer=CalculateTree()) Cheat ? 
-calc = calc_parser.parse
 
 
 def main():
@@ -74,12 +76,33 @@ def main():
         print(calc(s))
 
 def test():
-    string = "A + B + (T | !B) => B"
+    string = "A + B + B | T + B => B"
+#    string = "A | B | A <=> B"
+#    string = "A => S"
+#    string = "=ABG"
+#    string = "# ?ABG"
+#    string = "C + B => C# => C      # C AND B implies C"
+#    string = "C + R"
     print(string)
-    tree = calc(string)
-    print(tree.pretty())
-    for instr in tree.children:
-    	run_instruction(instr)
+    try:
+        tree = calc(string)
+    except:
+        print("Wrong file format")
+        return
+    print(tree.pretty("\033[1;32m--->\033[0m"))
+    print(tree.children[0].pretty("\033[1;32m--->\033[0m"))
+    print(tree.children[1].pretty("\033[1;32m--->\033[0m"))
+
+ #   t.children = tree.children[0]
+ #   for token in tree.children[1]
+ #       if (type(toke  == common.LETTER)
+ #           if (!fact_tree[token])
+ #               fact_tree[token].children[] = tree;
+ #           tree.children[1][k].children = tree.children[0]
+ #   tree.data = tree.children[1];
+ #   delete(tree.children[1]);
+  #  for instr in tree.children:
+   # 	run_instruction(instr)
 
 if __name__ == '__main__':
    test()
